339ad37fa77de2cc5e2813a198fd0dd6f42ef44e
Mihalis <mihalisfthenos@icloud.com>
2018-04-25 01:06:26 -0700

    Nest Routes in BrowserRouter

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Cryptocurrencies from '../pages/Cryptocurrencies'
 import Cryptocurrency from '../pages/Cryptocurrency'
 import AddIndustryToCryptocurrency from '../pages/AddIndustryToCryptocurrency'
@@ -10,16 +10,18 @@ import Login from '../pages/Login'
 import Logout from '../pages/Logout'
 
 const Main = () => (
-	<Switch>
-		<Route exact path="/" component={Cryptocurrencies}/>
-		<Route exact path="/about" component={About}/>
-		<Route exact path="/industries" component={Industries}/>
-		<Route exact path="/login" component={Login}/>
-		<Route exact path="/logout" component={Logout}/>
-		<Route exact path="/industries/:name" component={Industry}/>
-		<Route exact path="/cryptocurrencies/:symbol" component={Cryptocurrency}/>
-		<Route exact path="/industry-cryptocurrency" component={AddIndustryToCryptocurrency}/>
-	</Switch>
+	<Router>
+		<Switch>
+			<Route exact path="/" component={Cryptocurrencies}/>
+			<Route exact path="/about" component={About}/>
+			<Route exact path="/industries" component={Industries}/>
+			<Route exact path="/login" component={Login}/>
+			<Route exact path="/logout" component={Logout}/>
+			<Route exact path="/industries/:name" component={Industry}/>
+			<Route exact path="/cryptocurrencies/:symbol" component={Cryptocurrency}/>
+			<Route exact path="/industry-cryptocurrency" component={AddIndustryToCryptocurrency}/>
+		</Switch>
+	</Router>
 )
 
 export default Main
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 import './css/navbar.css'
 import cookie from 'react-cookies' 
 
@@ -15,15 +15,17 @@ export default function Navbar (props) {
           <img src="http://res.cloudinary.com/dattofkud/image/upload/v1524096719/cryptocat/cryptocroc/coincroc-brand.png"
                alt="coincroc-brandb" />
           </h1>
-          <ul className='links'>
-            <li><Link to="/">Cryptocurrencies</Link></li>
-            <li><Link to="/industries">Industries</Link></li>
-            {(!!accessToken) ? 
-              <li><Link to="/industry-cryptocurrency">Pair Industries</Link></li> :
-              null
-            }
-            <li><Link to="/about">About</Link></li>
-          </ul>
+          <Router>
+            <ul className='links'>
+              <li><Link to="/">Cryptocurrencies</Link></li>
+              <li><Link to="/industries">Industries</Link></li>
+              {(!!accessToken) ? 
+                <li><Link to="/industry-cryptocurrency">Pair Industries</Link></li> :
+                null
+              }
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </Router>
           <button>MENU</button>
           <ul className='hidden-links hidden'></ul>
         </nav>
